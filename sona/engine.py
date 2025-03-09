d3658d36ae90011725485dfe7610bf2a766e9bc7
interrrp <watchyourcache@gmail.com>
2025-03-09 14:54:00 +0800

    Implement bad minimax

--- sona/engine.py
+++ sona/engine.py
@@ -1,6 +1,8 @@
 import random
 
-from chess import Board, Move
+from chess import BLACK, WHITE, Board, Color, Move
+
+from sona.evaluator import evaluate
 
 
 class Engine:
@@ -18,4 +20,45 @@ class Engine:
         if is_first_move:
             return Move.from_uci("e2e4")
 
-        return random.choice(list(self._board.legal_moves))  # noqa: S311
+        color = self._board.turn
+        opposite_color = not color
+
+        legal_moves = list(self._board.legal_moves)
+
+        best_move = random.choice(legal_moves)  # noqa: S311
+        best_score: float = -1 if color == WHITE else 1
+
+        for move in legal_moves:
+            self._board.push(move)
+            score = self._minimax(3, opposite_color)
+            self._board.pop()
+
+            if (color == WHITE and score > best_score) or (color == BLACK and score < best_score):
+                best_score = score
+                best_move = move
+
+        return best_move
+
+    def _minimax(self, depth: int, color: Color, alpha: float = -1000, beta: float = 1000) -> float:
+        if depth == 0 or self._board.is_game_over():
+            return evaluate(self._board)
+
+        best_score: float = -1 if color == WHITE else 1
+
+        for move in self._board.legal_moves:
+            self._board.push(move)
+            score = self._minimax(depth - 1, not color, alpha, beta)
+            self._board.pop()
+
+            if (color == WHITE and score > best_score) or (color == BLACK and score < best_score):
+                best_score = score
+
+            if color == WHITE:
+                alpha = max(alpha, score)
+            else:
+                beta = min(beta, score)
+
+            if beta <= alpha:
+                break
+
+        return best_score
