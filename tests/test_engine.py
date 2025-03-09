0eb5cc776eb77659424d7c7cc72d6d67798b3e6b
interrrp <watchyourcache@gmail.com>
2025-03-09 11:59:05 +0800

    Add engine benchmark

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,4 +1,5 @@
 from chess import Board
+from pytest_benchmark.fixture import BenchmarkFixture
 
 from sona.engine import Engine
 
@@ -13,3 +14,8 @@ def test_engine_first_move_is_e4(board: Board, engine: Engine) -> None:
     engine.move()
     assert len(board.move_stack) == 1
     assert board.move_stack[0].uci() == "e2e4"
+
+
+def test_engine_performance(board: Board, engine: Engine, benchmark: BenchmarkFixture) -> None:
+    board.push_uci("e2e4")
+    benchmark(engine.move)
