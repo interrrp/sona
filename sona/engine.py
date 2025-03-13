e7381d5e543ab1901bb8c49fc8dcf444091285fa
interrrp <watchyourcache@gmail.com>
2025-03-13 21:05:30 +0800

    Proper implementation of transposition tables

--- sona/engine.py
+++ sona/engine.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import Literal
+
 from chess import Board, Move
 from chess.polyglot import zobrist_hash
 
@@ -5,10 +8,17 @@ from sona import INF
 from sona.evaluator import evaluate
 
 
+@dataclass(frozen=True)
+class TranspositionTableEntry:
+    score: float
+    depth: int
+    flag: Literal["exact", "lowerbound", "upperbound"]
+
+
 class Engine:
     def __init__(self, board: Board) -> None:
         self._board = board
-        self._score_cache: dict[int, float] = {}
+        self._transposition_table: dict[int, TranspositionTableEntry] = {}
         self.options: dict[str, str | int | bool] = {"Depth": 3}
 
     def move(self) -> Move:
@@ -36,27 +46,42 @@ class Engine:
 
         return best_move
 
-    def _negamax(self, depth: int, alpha: float = -INF, beta: float = INF) -> float:
+    def _negamax(self, depth: int, alpha: float = -INF, beta: float = INF) -> float:  # noqa: C901
         if depth == 0 or self._board.is_game_over():
             return evaluate(self._board)
 
         board_hash = zobrist_hash(self._board)
-        if score := self._score_cache.get(board_hash):
-            return score
+        if board_hash in self._transposition_table:
+            entry = self._transposition_table[board_hash]
+            if entry.depth >= depth:
+                if entry.flag == "exact" or alpha >= beta:
+                    return entry.score
+                if entry.flag == "lowerbound":
+                    alpha = max(alpha, entry.score)
+                elif entry.flag == "upperbound":
+                    beta = min(beta, entry.score)
 
-        score = -INF
+        original_alpha = alpha
+        best_score = -INF
         for move in self._board.legal_moves:
             self._board.push(move)
-            score = max(score, -self._negamax(depth - 1, -beta, -alpha))
+            score = -self._negamax(depth - 1, -beta, -alpha)
             self._board.pop()
 
+            best_score = max(best_score, score)
             alpha = max(alpha, score)
             if alpha >= beta:
                 break
 
-        self._score_cache[board_hash] = score
+        if best_score <= original_alpha:
+            flag = "upperbound"
+        elif best_score >= beta:
+            flag = "lowerbound"
+        else:
+            flag = "exact"
+        self._transposition_table[board_hash] = TranspositionTableEntry(best_score, depth, flag)
 
-        return score
+        return best_score
 
     @property
     def _depth(self) -> int:
